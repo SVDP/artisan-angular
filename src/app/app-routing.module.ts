9a09c70d56a6664bea39dd2c4ed50072608c7792
Samuel <samuel.vandeperre@gmail.com>
2019-05-16 16:25:03 +0200

    Slidemenu and Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,19 @@ import { NgModule } from '@angular/core'
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router'
 
 const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', loadChildren: './home/home.module#HomePageModule' }
+    {
+        path: '',
+        redirectTo: 'discover',
+        pathMatch: 'full'
+    },
+    {
+        path: 'discover',
+        loadChildren: './discover/discover.module#DiscoverPageModule'
+    },
+    {
+        path: 'match',
+        loadChildren: './match/match.module#MatchPageModule'
+    }
 ]
 
 @NgModule({
--- src/app/discover/discover.page.spec.ts
+++ src/app/discover/discover.page.spec.ts
@@ -1,21 +1,21 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { HomePage } from './home.page'
+import { DiscoverPage } from './discover.page'
 
-describe('HomePage', () => {
-    let component: HomePage
-    let fixture: ComponentFixture<HomePage>
+describe('DiscoverPage', () => {
+    let component: DiscoverPage
+    let fixture: ComponentFixture<DiscoverPage>
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [HomePage],
+            declarations: [DiscoverPage],
             schemas: [CUSTOM_ELEMENTS_SCHEMA]
         }).compileComponents()
     }))
 
     beforeEach(() => {
-        fixture = TestBed.createComponent(HomePage)
+        fixture = TestBed.createComponent(DiscoverPage)
         component = fixture.componentInstance
         fixture.detectChanges()
     })
