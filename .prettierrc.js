ab0f6d5ee96fea5e2739d5cc7cd380e895d1902e
Samuel <samuel.vandeperre@gmail.com>
2019-05-16 13:27:49 +0200

    Initial Setup

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    printWidth: 80,
+    singleQuote: true,
+    semi: false,
+    tabWidth: 4,
+    trailingComma: 'es5'
+}
