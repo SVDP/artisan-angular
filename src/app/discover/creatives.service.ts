c33e995216ff9d5ad7fd6be14924017b0028e13d
Samuel <samuel.vandeperre@gmail.com>
2019-05-23 23:33:20 +0200

    Content

--- src/app/discover/creatives.service.ts
+++ src/app/discover/creatives.service.ts
@@ -11,29 +11,67 @@ export class CreativesService {
             'c1',
             'Milica Tucakovic',
             'Student of Faculty of Fine Arts',
-            '',
-            '',
-            '',
             './assets/img/creatives/c1.jpg',
-            'https://mir-s3-cdn-cf.behance.net/projects/404/d9a33d77819747.Y3JvcCwxMjUzLDk4MCwyMjYsMA.png',
-            'https://mir-s3-cdn-cf.behance.net/projects/404/d9a33d77819747.Y3JvcCwxMjUzLDk4MCwyMjYsMA.png',
-            'https://mir-s3-cdn-cf.behance.net/projects/404/d9a33d77819747.Y3JvcCwxMjUzLDk4MCwyMjYsMA.png',
-            'https://mir-s3-cdn-cf.behance.net/projects/404/d9a33d77819747.Y3JvcCwxMjUzLDk4MCwyMjYsMA.png',
-            'https://mir-s3-cdn-cf.behance.net/projects/404/d9a33d77819747.Y3JvcCwxMjUzLDk4MCwyMjYsMA.png'
+            './assets/img/shots/s1.jpg',
+            './assets/img/shots/s2.jpg',
+            './assets/img/shots/s3.jpg',
+            './assets/img/shots/s4.jpg',
+            './assets/img/shots/s5.jpg'
         ),
         new Creative(
             'c2',
-            'Random name',
-            'Description of the user',
-            '',
-            '',
-            '',
+            'Tomek Bilic',
+            'Motion artist from Croatia',
             './assets/img/creatives/c2.jpg',
-            '',
-            '',
-            '',
-            '',
-            ''
+            './assets/img/shots/s6.jpg',
+            './assets/img/shots/s7.jpg',
+            './assets/img/shots/s8.jpg',
+            './assets/img/shots/s9.jpg',
+            './assets/img/shots/s10.jpg'
+        ),
+        new Creative(
+            'c3',
+            'Anna Zhou',
+            'Living in Bejing',
+            './assets/img/creatives/c3.jpg',
+            './assets/img/shots/s11.jpg',
+            './assets/img/shots/s12.jpg',
+            './assets/img/shots/s13.jpg',
+            './assets/img/shots/s14.jpg',
+            './assets/img/shots/s15.jpg'
+        ),
+        new Creative(
+            'c4',
+            "Jacqueline L' herminez",
+            'Illustrator',
+            './assets/img/creatives/c4.jpg',
+            './assets/img/shots/s16.jpg',
+            './assets/img/shots/s17.jpg',
+            './assets/img/shots/s18.jpg',
+            './assets/img/shots/s19.jpg',
+            './assets/img/shots/s20.jpg'
+        ),
+        new Creative(
+            'c5',
+            'Viktor Vaughn',
+            'Designer from Montreal',
+            './assets/img/creatives/c5.jpg',
+            './assets/img/shots/s21.jpg',
+            './assets/img/shots/s22.jpg',
+            './assets/img/shots/s23.jpg',
+            './assets/img/shots/s24.jpg',
+            './assets/img/shots/s25.jpg'
+        ),
+        new Creative(
+            'c6',
+            'Nicholas Craven',
+            'I love typography and copywriting',
+            './assets/img/creatives/c6.jpg',
+            './assets/img/shots/s26.jpg',
+            './assets/img/shots/s27.jpg',
+            './assets/img/shots/s28.jpg',
+            './assets/img/shots/s29.jpg',
+            './assets/img/shots/s30.jpg'
         )
     ]
 
