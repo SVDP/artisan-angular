c1802742054356193df7bd515420129ac94dcfaa
Samuel <samuel.vandeperre@gmail.com>
2019-05-23 16:24:25 +0200

    Profile Page and Updates

--- src/app/match/swipe/swipe.page.ts
+++ src/app/match/swipe/swipe.page.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core'
 
+import { CreativesService } from '../../discover/creatives.service'
+import { Creative } from '../../discover/creative.model'
+
 @Component({
     selector: 'app-swipe',
     templateUrl: './swipe.page.html',
     styleUrls: ['./swipe.page.scss']
 })
 export class SwipePage implements OnInit {
-    constructor() {}
+    loadedCreatives: Creative[]
 
-    ngOnInit() {}
+    constructor(private creativesService: CreativesService) {}
+
+    ngOnInit() {
+        this.loadedCreatives = this.creativesService.creatives
+    }
 }
